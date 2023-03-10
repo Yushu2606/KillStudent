4f0376c5c722100aa9d09e368c8ff79f52c0df07
星梦StarsDream <starsdream00@icloud.com>
2023-03-10 20:38:05 +0800

    更新

---
+++ src/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] processNames = { "StudentMain", "ProcessProtect", "Multi", "Student" };
+foreach (string processName in processNames)
+{
+    foreach (Process process in Process.GetProcessesByName(processName))
+    {
+        process.Kill();
+        process.WaitForExit();
+    }
+}
