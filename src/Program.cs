a2275082a7502ce3f8c653f17827af6c0ac7a199
予纾 <100036558+Yuisyuu@users.noreply.github.com>
2024-03-22 09:14:53 +0800

    fix: Fix kill speed

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,14 @@
 string[] processNames = ["StudentMain", "ProcessProtect", "Multi", "Student", "Smonitor", "REDAgent", "checkrs", "rscheck"];
+List<Process> processes = new();
 foreach (string processName in processNames)
 {
     foreach (Process process in Process.GetProcessesByName(processName))
     {
         process.Kill();
-        process.WaitForExit();
+        processes.Add(process);
     }
+}
+foreach (Process process in processes)
+{
+    process.WaitForExit();
 }
